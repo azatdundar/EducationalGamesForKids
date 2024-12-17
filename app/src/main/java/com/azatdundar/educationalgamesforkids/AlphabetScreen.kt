3aa9479a0928f9e313d2a88a266ca49b3368ae04
azatdundar <azatdundar.ad@gmail.com>
2024-12-17 16:54:27 +0300

    The structure of alphabet screen is changed.

--- app/src/main/java/com/azatdundar/educationalgamesforkids/AlphabetScreen.kt
+++ app/src/main/java/com/azatdundar/educationalgamesforkids/AlphabetScreen.kt
@@ -1,14 +1,22 @@
 package com.azatdundar.educationalgamesforkids
 
 import android.speech.tts.TextToSpeech
+import android.widget.Space
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.ArrowForward
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -32,17 +40,37 @@ import androidx.navigation.compose.rememberNavController
 @Composable
 fun AlphabetScreen(navController: NavHostController, tts: TextToSpeech?) {
 
-    var isSpeaking by remember { mutableStateOf(false) }
+    val alphabet = listOf(
+        'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
+    )
+
+    val sizeOfAlphabet = alphabet.size
+    var questionNumber by remember{ mutableStateOf(0) }
+    var letter :Char = alphabet[questionNumber]
 
 
-
-    Surface(modifier = Modifier.fillMaxSize()) {
+    Surface(modifier = Modifier.fillMaxSize(),
+        color = Color(red = 255, green = 223, blue = 186)) {
         Column(horizontalAlignment = Alignment.CenterHorizontally,
                 verticalArrangement = Arrangement.Center
             ) {
-            LetterButton(letter = "A")
-            LetterButton(letter = "B")
+            LetterButton(letter = letter)
+            Spacer(modifier = Modifier.padding(40.dp))
+            Row {
+                previousButton(numOfChar = sizeOfAlphabet, numOfQuestions = questionNumber) {
+                    if(questionNumber>0){
+                        questionNumber--
+                    }
+                }
+
+                nextButton(numOfChar = sizeOfAlphabet, numOfQuestions = questionNumber) {
+                    if(questionNumber<sizeOfAlphabet-1){
+                        questionNumber++
+                    }
+                }
+            }
         }
+
     }
 
 }
@@ -50,11 +78,9 @@ fun AlphabetScreen(navController: NavHostController, tts: TextToSpeech?) {
 
 
 @Composable
-fun LetterButton(letter : String){
-    var isSpeaking by remember { mutableStateOf(false) }
+fun LetterButton(letter : Char){
 
-
-    Button(modifier = Modifier.size(120.dp),
+    Button(modifier = Modifier.size(200.dp),
         shape = RoundedCornerShape(16.dp),
         onClick = {
             //*TODO*
@@ -67,19 +93,48 @@ fun LetterButton(letter : String){
 }
 
 @Composable
-fun LetterText(letter : String){
+fun LetterText(letter : Char){
     val gaamliFontFamily = FontFamily(
         Font(R.font.gaamli)
     )
 
 
 
-    Text(text = letter, fontSize = 60.sp,
+    Text(text = letter.toString(), fontSize = 60.sp,
         fontFamily = gaamliFontFamily,
         color = Color.Blue
         )
 }
 
+@Composable
+fun previousButton(numOfChar: Int, numOfQuestions:Int, onClick:()->Unit){
+    Button(onClick = { onClick() },
+        shape = CircleShape,
+        modifier = Modifier.size(80.dp),
+        colors = ButtonDefaults.buttonColors(containerColor =Color(0xFF4fa3e3))
+
+    ) {
+        Icon(imageVector = Icons.Default.ArrowBack, contentDescription = "Back",
+            tint = Color.Black,
+            modifier = Modifier.size(30.dp))
+    }
+
+}
+
+@Composable
+fun nextButton(numOfChar : Int, numOfQuestions:Int, onClick:()->Unit){
+    Button(onClick = {
+            onClick()
+    },
+        shape = CircleShape,
+        modifier = Modifier.size(80.dp),
+        colors = ButtonDefaults.buttonColors(containerColor =Color(0xFF4fa3e3))
+        ) {
+        Icon(imageVector = Icons.Default.ArrowForward, contentDescription = "Back",
+            tint = Color.Black,
+            modifier = Modifier.size(30.dp))
+    }
+}
 @Preview(showBackground = true, name = "Alphabet Screen Preview")
 @Composable
 fun PreviewAlphabetScreen() {
